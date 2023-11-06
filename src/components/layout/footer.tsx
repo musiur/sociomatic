d51492cd2196c233ab79a643d1d8ceadc8d8a8fd
musiur <musiur.opu@gmail.com>
2023-11-07 04:04:51 +0600

    [Feature] Footer: Background Illustration added #7

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,11 @@
 const Footer = () => {
-    return (
-        <footer className="bg-muted">
-            <div className="container section">Footer</div>
-        </footer>
-    )
-}
+  return (
+    <footer className="bg-muted">
+      <div className="bg-[url('/images/backgrounds/WhiteGrid.svg')] bg-top bg-cover">
+        <div className="container section">Footer</div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
