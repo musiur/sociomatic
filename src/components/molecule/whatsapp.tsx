084aaa2ec4dff26ac77d462d364c521e31b4bfad
musiur <musiur.opu@gmail.com>
2023-11-23 05:37:49 +0600

    [Feature] Support: Whatsapp #31

--- src/components/molecule/whatsapp.tsx
+++ src/components/molecule/whatsapp.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const WhatsApp = () => {
   return (
-      <Link href="https://wa.me/+971567268607" passHref={true} target="_blank" className="fixed bottom-[100px] right-[30px] border border-secondarymuted shadow-lg rounded-full p-[15px] hover:bg-secondary group">
+      <Link href="https://wa.me/+971567268607" passHref={true} target="_blank" className="fixed bottom-[100px] right-[30px] border border-secondarymuted shadow-lg rounded-full p-[15px] bg-white hover:bg-secondary group">
         <MessageCircle className="w-[30px] h-[30px] stroke-secondary group-hover:stroke-white"/>
       </Link>
   );
