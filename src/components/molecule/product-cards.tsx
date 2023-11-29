bea64fa7b9417764003be59f325e94242a1d6582
MusiurAlamOpu <musiur.opu@gmail.com>
2023-11-29 16:14:13 +0600

    [Enhancement] UI UX case studies Content #65

--- src/components/molecule/product-cards.tsx
+++ src/components/molecule/product-cards.tsx
@@ -3,6 +3,7 @@
 import { MoveRight } from "lucide-react";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import GetAQuote from "./get-a-quote";
 
 const ProductCards = ({ params }: { params: { slug: string } }) => {
   let data: any = WordpressProducts;
@@ -43,12 +44,16 @@ const ProductCards = ({ params }: { params: { slug: string } }) => {
                       {title}
                     </h5>
                     <p>{description}</p>
-                    <Link href={link} passHref={true} target="_blank">
-                      <Button variant={"outline"} className="group gap-[8px]">
-                        Explore&nbsp;
-                        <MoveRight className="stroke-[1.3px] w-[16px] stroke-primary group-hover:stroke-secondary" />
-                      </Button>
-                    </Link>
+                    {params.slug.includes("ui-ux-and-graphic-design") ? (
+                      <GetAQuote />
+                    ) : (
+                      <Link href={link} passHref={true} target="_blank">
+                        <Button variant={"outline"} className="group gap-[8px]">
+                          Explore&nbsp;
+                          <MoveRight className="stroke-[1.3px] w-[16px] stroke-primary group-hover:stroke-secondary" />
+                        </Button>
+                      </Link>
+                    )}
                   </div>
                 </div>
               );
@@ -105,35 +110,35 @@ const UiUxProducts = [
     {
       id: 1,
       image: "bg-[url('/images/projects/uiux/uiuxDashboard.png')]",
-      title: "WellDev",
+      title: "Dashboard Design",
       description:
-        "Incididunt minim id dolor et sint esse. Tempor occaecat ex dolore nisi enim nulla labore dolore. In laboris cillum et pariatur ut fugiat dolor ad id occaecat. Et reprehenderit      adipisicing non aliquip.",
+        "Comprehensive Dashboard UI design with multi function of any sorts of software field e.g. Ecommerce, Fintech, Edtech etc.",
       link: "",
     },
     {
       id: 2,
       image: "bg-[url('/images/projects/uiux/uiuxDoyos.png')]",
-      title: "WellDev",
+      title: "Doyos",
       description:
-        "Incididunt minim id dolor et sint esse. Tempor occaecat ex dolore nisi enim nulla labore dolore. In laboris cillum et pariatur ut fugiat dolor ad id occaecat. Et reprehenderit      adipisicing non aliquip.",
+        "A Robust UI and UX for a Trading Company having several landing pages and feature pages ensuring customer centric design principles",
       link: "",
     },
   ],
   [
     {
       id: 1,
-      image: "bg-[url('/images/projects/wordpress/uiuxSociomatic.png')]",
-      title: "WellDev",
+      image: "bg-[url('/images/projects/uiux/uiuxSociomatic.png')]",
+      title: "Sociomatic",
       description:
-        "Incididunt minim id dolor et sint esse. Tempor occaecat ex dolore nisi enim nulla labore dolore. In laboris cillum et pariatur ut fugiat dolor ad id occaecat. Et reprehenderit      adipisicing non aliquip.",
+        "This is the site where you are right now designed with care and built with keen details to the minimalistic design approach.",
       link: "",
     },
     {
       id: 2,
-      image: "bg-[url('/images/projects/wordpress/project.png')]",
-      title: "WellDev",
+      image: "bg-[url('/images/projects/uiux/uiuxEssential.png')]",
+      title: "Essential",
       description:
-        "Incididunt minim id dolor et sint esse. Tempor occaecat ex dolore nisi enim nulla labore dolore. In laboris cillum et pariatur ut fugiat dolor ad id occaecat. Et reprehenderit      adipisicing non aliquip.",
+        "An API providing platform landing page with modern UI and UX design",
       link: "",
     },
   ],
@@ -176,42 +181,41 @@ const CustomWebProducts = [
     },
   ],
 ];
-
 const SoftwareProducts = [
   [
     {
       id: 1,
-      image: "bg-[url('/images/projects/uiux/uiuxDashboard.png')]",
-      title: "WellDev",
+      image: "bg-[url('/images/projects/custom-web/easypay.png')]",
+      title: "Doyos",
       description:
-        "Incididunt minim id dolor et sint esse. Tempor occaecat ex dolore nisi enim nulla labore dolore. In laboris cillum et pariatur ut fugiat dolor ad id occaecat. Et reprehenderit      adipisicing non aliquip.",
-      link: "",
+        "Doyos provide the proper trading environment to the clients. Doyos offer high quality order execution, no commission on deposits and withdrawals and wide selection of instruments as we believe that these are the base of the best trading strategies.",
+      link: "https://doyos.vercel.app",
     },
     {
       id: 2,
-      image: "bg-[url('/images/projects/uiux/uiuxDoyos.png')]",
-      title: "WellDev",
+      image: "bg-[url('/images/projects/custom-web/houses.png')]",
+      title: "Musiur",
       description:
-        "Incididunt minim id dolor et sint esse. Tempor occaecat ex dolore nisi enim nulla labore dolore. In laboris cillum et pariatur ut fugiat dolor ad id occaecat. Et reprehenderit      adipisicing non aliquip.",
-      link: "",
+        "Technical blog site of Software Engineer Musiur Alam Opu. There he  tries to put all the knowledges that he experienced during his learning to working in the industry and so on.",
+      link: "https://musiur-blog.vercel.app",
     },
   ],
   [
     {
       id: 1,
-      image: "bg-[url('/images/projects/wordpress/uiuxSociomatic.png')]",
-      title: "WellDev",
+      image: "bg-[url('/images/projects/custom-web/laundrycare.png')]",
+      title: "LaundryCare",
       description:
-        "Incididunt minim id dolor et sint esse. Tempor occaecat ex dolore nisi enim nulla labore dolore. In laboris cillum et pariatur ut fugiat dolor ad id occaecat. Et reprehenderit      adipisicing non aliquip.",
-      link: "",
+        "We, Laundry Care, dedicated group of people working for goodness and make easier to manage laundry services in Bashundhara Residential Area. Any shop can join us to provide services and anybody can be a Rider Partner with us. We are making a HUB that will connect all the Laundry services provider around the Residential Area.",
+      link: "https://laundrycare.vercel.app/en/user",
     },
     {
       id: 2,
-      image: "bg-[url('/images/projects/wordpress/project.png')]",
-      title: "WellDev",
+      image: "bg-[url('/images/projects/custom-web/wsl.png')]",
+      title: "1link",
       description:
-        "Incididunt minim id dolor et sint esse. Tempor occaecat ex dolore nisi enim nulla labore dolore. In laboris cillum et pariatur ut fugiat dolor ad id occaecat. Et reprehenderit      adipisicing non aliquip.",
-      link: "",
+        "The one link your readers will need to find you and your books. Showcase your books on a page with the colors and style you want.",
+      link: "https://1linkf.vercel.app",
     },
   ],
 ];
