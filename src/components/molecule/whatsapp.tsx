9e5e51ac6aa90b300c1ac339e25ac6d9f8f4af0e
MusiurAlamOpu <musiur.opu@gmail.com>
2023-12-05 16:02:10 +0600

    [Enhancement] What'App number in Contact us page #80

--- src/components/molecule/whatsapp.tsx
+++ src/components/molecule/whatsapp.tsx
@@ -1,5 +1,6 @@
-import { MessageSquare } from "lucide-react";
+// import { MessageSquare } from "lucide-react";
 import Link from "next/link";
+import WhatsAppIcon from "../assets/whatsapp";
 
 const WhatsApp = () => {
   return (
@@ -9,7 +10,8 @@ const WhatsApp = () => {
       target="_blank"
       className="fixed bottom-[100px] right-[30px] group border border-secondary bg-secondary hover:bg-indigo-700 h-[60px] w-[60px] rounded-full flex items-center justify-center hover:shadow-2xl"
     >
-      <MessageSquare className="w-[30px] h-[30px] stroke-white" />
+      {/* <MessageSquare className="w-[30px] h-[30px] stroke-white" /> */}
+      <WhatsAppIcon className="w-[40px] h-[40px] stroke-white stroke-[1.6px]" />
     </Link>
   );
 };
