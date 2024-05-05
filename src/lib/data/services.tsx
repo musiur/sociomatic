e48790764f771f88b0eae3f4c40a27cbc500e15c
musiur <musiur.opu@gmail.com>
2024-05-06 01:30:06 +0600

    google analytics content added without FAQ

--- src/lib/data/services.tsx
+++ src/lib/data/services.tsx
@@ -2000,4 +2000,319 @@ export const ServicePageCOPY = {
       ],
     },
   },
+  googleAnalytics: {
+    hero: {
+      tagline: <>Get Trust in Your Arsenal</>,
+      title: (
+        <>
+          Unlock User Insights & Retention: The Power of GA4 & Google Tag
+          Manager
+        </>
+      ),
+      body: (
+        <>
+          Need help understanding your website visitors? Discover a dynamic duo
+          - Google Analytics 4 (GA4) and Google Tag Manager (GTM) - that unlocks
+          powerful user insights to optimize your marketing funnel and boost
+          retention.
+        </>
+      ),
+      cta: {
+        primary: {
+          text: <>Get Started Right Away</>,
+          link: "/",
+        },
+        secondary: {
+          text: <>Get A Free Consultation</>,
+          link: "/",
+        },
+      },
+    },
+    limitedOffer: {
+      tagline: <></>,
+      title: (
+        <>
+          <span>Redefine!</span> User Engagement in 2024
+        </>
+      ),
+      body: {
+        para: (
+          <>
+            Sign up for our free one-on-one consultation today and receive a
+            customized GA4 and GTM implementation plan (valued at $250) to
+            jumpstart your user acquisition and retention strategy!
+          </>
+        ),
+        benefits: [],
+      },
+      cta: {
+        primary: {
+          text: <>Get Started Right Away</>,
+          link: "/",
+        },
+        secondary: {
+          text: <>Get A Free Consultation</>,
+          link: "/",
+        },
+      },
+    },
+    whyWeAndWhatWeDo: {
+      tagline: <>Designing Experiences, Inspiring Engagement</>,
+      title: <>Why Choose Us?</>,
+      body: {
+        para: (
+          <>
+            We are experts in guiding approximately 400 businesses through GA4
+            and GTM. Here&apos;s what sets us apart
+          </>
+        ),
+        benefits: [
+          {
+            id: 1,
+            icon: <></>,
+            title: <>Deep Expertise</>,
+            paragraph: (
+              <>
+                Our team possesses in-depth knowledge of GA4 and GTM, ensuring
+                optimal implementation for your needs.
+              </>
+            ),
+          },
+          {
+            id: 2,
+            icon: <></>,
+            title: <>Streamlined Approach</>,
+            paragraph: (
+              <>
+                We simplify the setup process, saving you valuable time and
+                resources.
+              </>
+            ),
+          },
+          {
+            id: 3,
+            icon: <></>,
+            title: <>Actionable Insights</>,
+            paragraph: (
+              <>
+                We go beyond data collection, helping you translate insights
+                into actionable marketing strategies
+              </>
+            ),
+          },
+          {
+            id: 4,
+            icon: <></>,
+            title: <>Customizable Solutions</>,
+            paragraph: (
+              <>
+                We tailor our approach to your unique website and marketing
+                goals.
+              </>
+            ),
+          },
+          {
+            id: 5,
+            icon: <></>,
+            title: <>Ongoing Support</>,
+            paragraph: (
+              <>
+                We offer ongoing guidance to ensure you continue to leverage the
+                full potential of GA4 and GTM.
+              </>
+            ),
+          },
+          {
+            id: 6,
+            icon: <></>,
+            title: <>Proven Results</>,
+            paragraph: (
+              <>
+                Our track record speaks for itself - we&apos;
+                <span>significantly</span>&apos;help 400 businesses improve user
+                acquisition and retention.
+              </>
+            ),
+          },
+        ],
+      },
+    },
+    userEmpathy: {
+      tagline: <>Empathetic Support Always</>,
+      title: (
+        <>
+          We understand the challenges of attracting and retaining website
+          visitors
+        </>
+      ),
+      body: (
+        <>
+          We understand the challenges of attracting and retaining website
+          visitors. It would help to have a clear picture of user behavior to
+          make data-driven decisions. GA4 and GTM can be your secret weapon!
+        </>
+      ),
+    },
+    coreBenefits: {
+      tagline: <>Inspiring Engagement</>,
+      title: <>Unlock the Power of User Insights</>,
+      body: {
+        para: (
+          <>
+            Go beyond basic website analytics and gain a complete understanding
+            of how users interact with your website. Track user journeys,
+            identify pain points, and discover hidden opportunities to optimize
+            your marketing funnel
+          </>
+        ),
+        benefits: [
+          {
+            id: 1,
+            title: <>Enhanced User Tracking</>,
+            para: (
+              <>
+                Gain a comprehensive understanding of user behavior with
+                GA4&apos;s advanced tracking capabilities
+              </>
+            ),
+          },
+          {
+            id: 2,
+            title: <>Targeted Marketing</>,
+            para: (
+              <>
+                Use GTM to deliver personalized experiences that resonate with
+                your audience.
+              </>
+            ),
+          },
+          {
+            id: 3,
+            title: <>Data-Driven Decisions</>,
+            para: (
+              <>
+                Make informed marketing decisions based on real-time user data
+                and insights.
+              </>
+            ),
+          },
+          {
+            id: 4,
+            title: <>Improved Retention Strategies</>,
+            para: (
+              <>
+                Identify user drop-off points and optimize your funnel for
+                improved engagement and loyalty
+              </>
+            ),
+          },
+        ],
+      },
+    },
+    endingFunnel: {
+      tagline: <>Unleash the Potential of Your Website with GA4 & GTM!</>,
+      title: (
+        <>
+          Master User Acquisition & Retention with Google&apos;s Powerful Tools
+        </>
+      ),
+      body: (
+        <>
+          Ready to take your website to the next level? Let us help you leverage
+          the combined power of Google Analytics 4 and Google Tag Manager for a
+          <span>guaranteed tracking solution with a money-back guarantee.</span>
+          Get started for your free one-on-one consultation today and unlock
+          valuable user insights that will transform your marketing strategy!
+        </>
+      ),
+      cta: {
+        primary: {
+          text: <>Get Started Right Away</>,
+          link: "/",
+        },
+        secondary: {
+          text: <>Get A Free Consultation</>,
+          link: "/",
+        },
+      },
+    },
+    faq: {
+      title: <>Frequently Asked Questions</>,
+      para: (
+        <>
+          Are you confused by GA4, GTM, Looker Studio, and Google Ads Tracking?
+          We&apos;ve Got You Covered!
+        </>
+      ),
+      faqs: [
+        {
+          id: 1,
+          question: <>What&apos;s GA4?</>,
+          answer: (
+            <>
+              Google Analytics 4, the latest website and app analytics platform,
+              tracks user journeys across platforms and provides future-proof
+              data.
+            </>
+          ),
+          tip: (
+            <>
+              We&apos;ll help you set up GA4 and ensure you capture the data
+              that matters most.
+            </>
+          ),
+        },
+        {
+          id: 2,
+          question: <>Can Sociomatic design unique visuals for my company?</>,
+          answer: (
+            <>
+              Sociomatic provides customized graphic design services, such as
+              logo creation, branding materials, and images that match your
+              company&apos;s identity.
+            </>
+          ),
+        },
+        {
+          id: 3,
+          question: (
+            <>
+              How does UI/UX design affect the functionality of an app or
+              website?
+            </>
+          ),
+          answer: (
+            <>
+              Good UI/UX design boosts user happiness, lowers bounce rates, and
+              enables more straightforward user navigation, contributing to
+              better performance.
+            </>
+          ),
+        },
+        {
+          id: 4,
+          question: <>What graphic design tools does Sociomatic employ?</>,
+          answer: (
+            <>
+              Sociomatic uses industry-standard software like Adobe Creative
+              Suite to guarantee expert and high-caliber graphic design results.
+            </>
+          ),
+        },
+        {
+          id: 5,
+          question: (
+            <>Can Sociomatic rework the website or app I already have?</>
+          ),
+          answer: (
+            <>
+              Sociomatic does offer redesign services, guaranteeing a
+              contemporary appearance while preserving or improving
+              functionality.
+            </>
+          ),
+        },
+      ],
+    },
+  },
 };
