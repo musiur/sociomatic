74c23b3a5e6046fbfe9cceb834e128b70745bb4a
musiur <musiur.opu@gmail.com>
2024-05-20 19:32:02 +0600

    [feature] Funnel First Step Email Taking with Verification #146

--- src/components/molecule/email-modal.tsx
+++ src/components/molecule/email-modal.tsx
@@ -35,31 +35,30 @@ const EmailModal = ({ buttonText }: { buttonText: ReactElement }) => {
           {buttonText}
         </span>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="max-w-[300px] sm:max-w-[425px]">
         <Image
           src="/images/backgrounds/blue-logo-gradiant-light-bg-tb.svg"
           alt=""
           fill
-          className="object-contain object-top"
+          className="w-full object-contain object-top"
         />
-        <DialogHeader className="space-y-4 pt-[240px] z-10">
-          {step === 1 ? (
-            <DialogTitle className="text-xl md:text-2xl font-normal">
-              Join a small group of&nbsp;
-              <span className="font-bold text-xl md:text-2xl">
-                elite entrepreneurs
-              </span>
-              &nbsp;today
-            </DialogTitle>
-          ) : null}
+        <DialogHeader className="space-y-2 pt-[180px] md:pt-[240px] z-10">
+          <DialogTitle className="text-xl md:text-2xl font-normal">
+            {step === 1 ? "Join a small group of" : "Email"}&nbsp;
+            <span className="font-bold text-xl md:text-2xl">
+              {step === 1 ? "elite entrepreneurs" : "Verification"}
+            </span>
+            &nbsp;{step === 1 ? "today" : null}
+          </DialogTitle>
+
           <DialogDescription className="text-gray-400 z-10">
             {step === 1
               ? "And finally create the life, freedom, and wealth you want!"
               : "We have sent an OTP to your email. Please Enter the OTP below to verify you email account!"}
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4 z-10">
-          <div className="space-y-4">
+        <div className="grid gap-4 pb-4 z-10">
+          <div className="flex items-center justify-center sm:justify-start gap-2">
             {step === 1 ? (
               <Input
                 id="email"
@@ -69,7 +68,7 @@ const EmailModal = ({ buttonText }: { buttonText: ReactElement }) => {
               />
             ) : (
               <InputOTP
-                maxLength={6}
+                maxLength={4}
                 value={otp}
                 onChange={(value: any) => setOtp(value)}
               >
@@ -78,8 +77,6 @@ const EmailModal = ({ buttonText }: { buttonText: ReactElement }) => {
                   <InputOTPSlot index={1} />
                   <InputOTPSlot index={2} />
                   <InputOTPSlot index={3} />
-                  <InputOTPSlot index={4} />
-                  <InputOTPSlot index={5} />
                 </InputOTPGroup>
               </InputOTP>
             )}
@@ -96,9 +93,9 @@ const EmailModal = ({ buttonText }: { buttonText: ReactElement }) => {
               {step === 1 ? "Get Started" : "Verify Email"}
             </Button>
             <DialogClose className="w-full">
-            <span className="bg-white text-center h-[38px] rounded-lg border flex items-center justify-center px-6 font-medium sm:cursor-pointer">
-          Close
-        </span>
+              <span className="bg-white text-center h-[38px] rounded-lg border flex items-center justify-center px-6 font-medium sm:cursor-pointer">
+                Close
+              </span>
             </DialogClose>
           </div>
         </DialogFooter>
