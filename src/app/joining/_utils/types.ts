a23afc2c53ca9d6c0c82db3a8b76d5079a20c533
musiur <musiur.opu@gmail.com>
2024-08-19 23:10:55 +0600

    type and validation added to joining form

--- src/app/joining/_utils/types.ts
+++ src/app/joining/_utils/types.ts
@@ -1,20 +1,22 @@
 import { z } from "zod";
 
+const T__ArrayOfString = z.array(z.string().min(1));
+
 export const GoogleAdsFunnelForm = z.object({
     name: z.string().min(1),
     country: z.string().min(1),
     phone: z.string().min(1),
     website: z.string().optional(),
-    businessType: z.array(z.string().min(1)),
-    industryType: z.array(z.string().min(1)),
-    goals: z.array(z.string().min(1)),
+    businessType: T__ArrayOfString,
+    industryType: T__ArrayOfString,
+    goals: T__ArrayOfString,
     customGoals: z.string().optional(),
     challengesFaced: z.string().min(1),
     budget: z.number().min(1),
     workExperience: z.string().min(1).default("No"),
     workExperienceDetails: z.string().optional(),
     painpoints: z.string(),
-    commitment: z.array(z.string().min(1))
+    commitment: T__ArrayOfString
 });
 
 export type TGoogleAdsFunnelForm = z.infer<typeof GoogleAdsFunnelForm>
@@ -25,14 +27,14 @@ export const GoogleAnalyticsFunnelForm = z.object({
     country: z.string().min(1),
     phone: z.string().min(1),
     website: z.string().optional(),
-    industryType: z.array(z.string().min(1)),
+    industryType: T__ArrayOfString,
     challengesFaced: z.string().min(1),
     tools: z.string().min(1).default("No"),
     toolsUsed: z.string().optional(),
     xpGA4GTM: z.string().min(1).default("No"),
     goals: z.string().min(1),
     customGoals: z.string().optional(),
-    platformToSetup: z.array(z.string().min(1)),
+    platformToSetup: T__ArrayOfString,
 });
 
 export type TGoogleAnalyticsFunnelForm = z.infer<typeof GoogleAnalyticsFunnelForm>
@@ -43,18 +45,18 @@ export const WebDevelopmentFunnelForm = z.object({
     country: z.string().min(1),
     phone: z.string().min(1),
     website: z.string().optional(),
-    businessType: z.array(z.string().min(1)),
-    industryType: z.array(z.string().min(1)),
+    businessType: T__ArrayOfString,
+    industryType: T__ArrayOfString,
     customIndustry: z.string().optional(),
-    goals: z.string().min(1),
-    customGoals: z.array(z.string().min(1)),
+    goals: T__ArrayOfString,
+    customGoals: T__ArrayOfString,
     challengesFaced: z.string().min(1),
     budget: z.number().min(1),
     workExperience: z.string().min(1).default("No"),
     workExperienceDetails: z.string().optional(),
     customerAsPersona: z.string().min(1),
     painpoints: z.string(),
-    commitment: z.array(z.string().min(1))
+    commitment: T__ArrayOfString
 });
 
 export type TWebDevelopmentFunnelForm = z.infer<typeof WebDevelopmentFunnelForm>
@@ -64,7 +66,7 @@ export const SocialMediaAdsServicePageDataForm = z.object({
     country: z.string().min(1),
     phone: z.string().min(1),
     website: z.string().optional(),
-    platformType: z.array(z.string().min(1)),
+    platformType: T__ArrayOfString,
     customPlatform: z.string().optional(),
     goals: z.array(z.string().min(1)),
     customGoals: z.string().optional(),
