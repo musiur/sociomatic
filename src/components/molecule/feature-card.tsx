f5f97ab13819664c78f935f73210c1f2a52258c0
musiur <musiur.opu@gmail.com>
2024-10-06 14:52:30 +0600

    make icon stroke 2px of service card

--- src/components/molecule/feature-card.tsx
+++ src/components/molecule/feature-card.tsx
@@ -17,7 +17,6 @@ const FeatureCard = ({
   list: any;
   link: string;
 }) => {
-  // max-w-[365px]
   return (
     <ANIM__FadeInOutOnScroll className="rounded-[10px] border border-secondary-muted hover:border-secondary bg-white/5 backdrop-blur-[8px] hover:backdrop-blur-[8px] w-full px-[25px] py-[50px] shadow-[0_4px_25px_0_rgba(89,86,255,0.05)] transition ease-in-out duration-500 hover:scale-[1.05]">
       <div className="flex flex-col items-start justify-start small-gap">
@@ -35,7 +34,7 @@ const FeatureCard = ({
                 key={index}
                 className="flex items-start justify-start gap-[6.7px]"
               >
-                <BadgeCheck className="min-h-[16px] min-w-[16px] max-w-[16px] stroke-secondary stroke-[1.3px]" />
+                <BadgeCheck className="min-h-[16px] min-w-[16px] max-w-[16px] stroke-secondary stroke-[2px]" />
                 &nbsp;
                 {item}
               </li>
