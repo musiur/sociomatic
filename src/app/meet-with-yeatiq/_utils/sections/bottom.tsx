d3468c5107e1de6d3b0c3e578d54a3cff8b7d4b2
musiur <musiur.opu@gmail.com>
2025-01-09 13:47:15 +0600

    update bottom section of meet with yeatiq

--- src/app/meet-with-yeatiq/_utils/sections/bottom.tsx
+++ src/app/meet-with-yeatiq/_utils/sections/bottom.tsx
@@ -4,6 +4,33 @@ import Image from "next/image";
 import Link from "next/link";
 
 const LandingBottomSection = () => {
+  const Links = [
+    {
+      id: 2,
+      icon: (
+        <Phone className="w-4 h-4 min-w-4 stroke-secondary" />
+      ),
+      text: "+971507477541",
+      href: "tel:+971507477541",
+    },
+    {
+      id: 1,
+      icon: (
+        <Mail className="w-4 h-4 min-w-4 stroke-secondary" />
+      ),
+      text: "hello@thesociomatic.com",
+      href: "/contact-us",
+    },
+
+    {
+      id: 3,
+      icon: (
+        <WhatsAppIcon className="w-4 h-4 min-w-4 stroke-[2px] stroke-secondary" />
+      ),
+      text: "+971507477541 (WhatsApp)",
+      href: "https://wa.me/+971507477541",
+    },
+  ];
   return (
     <div className="container section grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
       <Image
@@ -14,32 +41,18 @@ const LandingBottomSection = () => {
         className="rounded-[40px] max-w-md shadow-2xl"
       />
       <div className="space-y-4">
-        <p className="inline-block px-4 py-1 rounded-xl border">
-          Contact Me
-        </p>
+        <p className="inline-block px-4 py-1 rounded-xl border">Contact Me</p>
         <h2>Have Questions?</h2>
         <p>Reach out anytime—I&apos;m happy to help!</p>
-        <ul className="space-y-2">
-          <Link href="/contact-us">
-          
-          
-          <li className="flex items-center gap-2">
-            <Mail className="w-6 h-6 min-w-6 bg-secondary/10 stroke-secondary rounded-full p-1" />{" "}
-            hello@thesociomatic.com
-          </li>
+        <ul className="space-y-2 flex flex-col gap-2">
+          {Links.map((link) => (
+            <Link href={link.href} key={link.id}>
+              <li className="px-2 py-1 rounded-xl border border-b-4 inline-flex items-center gap-2 hover:shadow-xl hover:border-secondary hover:border-b-4 transaction duration-300 ease-in-out">
+                {link.icon}
+                {link.text}
+              </li>
             </Link>
-          <li className="flex items-center gap-2">
-            <Phone className="w-6 h-6 min-w-6 bg-secondary/10 stroke-secondary rounded-full p-1" />{" "}
-            +971507477541
-          </li>
-          <li>
-            <Link
-      href="https://wa.me/+971507477541"
-      passHref={true}
-      target="_blank" className="flex items-center gap-2">
-            <WhatsAppIcon className="w-6 h-6 min-w-6 bg-secondary/10 stroke-[2px] stroke-secondary rounded-full p-1" />{" "}
-             +971507477541 (WhatsApp)
-          </Link>         </li>
+          ))}
         </ul>
       </div>
     </div>
